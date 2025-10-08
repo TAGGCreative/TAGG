9470ce187151deed03b14ec14b0b7041b907dbfd
TAGGCreative <96558822+TAGGCreative@users.noreply.github.com>
2025-10-07 22:29:31 -0700

    Handle missing Vimeo animated thumbnails

--- components/WorkThumb.js
+++ components/WorkThumb.js
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useState } from "react"
 import styled from "styled-components"
 
 const Image = styled.img`
@@ -11,7 +12,8 @@ const Gif = styled.img`
   width: 100%;
   border-radius: 5px;
   transition: opacity 0.2s ease-in-out;
-  outline: 1px solid var(--red);
+  outline: ${({ $hasAnimated }) =>
+    $hasAnimated ? "1px solid var(--red)" : "none"};
   opacity: 0;
 `
 
@@ -24,14 +26,92 @@ const Frame = styled.div`
   width: 100%;
 `
 
+const getBestStaticImage = (images = []) => {
+  if (!images?.length) {
+    return ""
+  }
+
+  const preferredIndexes = [3, 2, images.length - 1, 0]
+  for (const index of preferredIndexes) {
+    const candidate = images[index]?.link
+    if (candidate) {
+      return candidate
+    }
+  }
+
+  const firstAvailable = images.find(({ link }) => Boolean(link))
+  return firstAvailable?.link ?? ""
+}
+
+const getAnimatedCandidate = (thumb) => {
+  if (!thumb) {
+    return null
+  }
+
+  if (thumb?.sizes?.length) {
+    const { sizes } = thumb
+    const preferred = [...sizes].reverse().find(({ link }) => Boolean(link))
+    if (preferred?.link) {
+      return preferred.link
+    }
+  }
+
+  return thumb?.link ?? null
+}
+
 const WorkThumb = ({ images, thumb }) => {
-  const imageSrc = images[3]?.link
-  const desktopSizeGif = thumb?.sizes[2].link
+  const staticImageSrc = useMemo(() => getBestStaticImage(images), [images])
+  const animatedCandidate = useMemo(
+    () => getAnimatedCandidate(thumb),
+    [thumb],
+  )
+
+  const [hoverSrc, setHoverSrc] = useState(staticImageSrc)
+  const [hasAnimated, setHasAnimated] = useState(false)
+
+  useEffect(() => {
+    setHoverSrc(staticImageSrc)
+    setHasAnimated(false)
+  }, [staticImageSrc])
+
+  useEffect(() => {
+    if (!animatedCandidate || animatedCandidate === staticImageSrc) {
+      setHasAnimated(false)
+      return
+    }
+
+    let isMounted = true
+    const preload = new window.Image()
+
+    preload.onload = () => {
+      if (!isMounted) return
+      setHoverSrc(animatedCandidate)
+      setHasAnimated(true)
+    }
+
+    preload.onerror = () => {
+      if (!isMounted) return
+      setHoverSrc(staticImageSrc)
+      setHasAnimated(false)
+    }
+
+    preload.src = animatedCandidate
+
+    return () => {
+      isMounted = false
+    }
+  }, [animatedCandidate, staticImageSrc])
 
   return (
     <Frame>
-      <Gif src={desktopSizeGif} alt={imageSrc} className="gif" />
-      <Image src={imageSrc} className="image" />
+      <Gif
+        src={hoverSrc}
+        alt=""
+        aria-hidden="true"
+        className="gif"
+        $hasAnimated={hasAnimated}
+      />
+      <Image src={staticImageSrc} alt="" className="image" />
     </Frame>
   )
 }
